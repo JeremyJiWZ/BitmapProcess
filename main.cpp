dbf5ebbdc953b0dab81d3ced25effde371e7441e
JeremyJiWZ <mr.ji@icloud.com>
2015-11-26 21:48:51 +0800

    add some test picture

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@
 using namespace std;
 int main(int argc, const char * argv[]) {
     Bitmap image;
-    string fileName="/Users/jiwentadashi/Pragramming/ImageProcessI/ImageProcessI/10g.bmp";
-    string des_file="/Users/jiwentadashi/Desktop/equalization.bmp";
+    string fileName="/Users/jiwentadashi/Pragramming/ImageProcessI/ImageProcessI/gray3.bmp";
+    string des_file="/Users/jiwentadashi/Desktop/enhance.bmp";
     image.setName(fileName);
     image.ReadImage();
 //    image.TurnToGray();
-//    image.VisibilityEnhancement();
-    image.HistogramEqualization(); 
+    image.VisibilityEnhancement();
+//    image.HistogramEqualization();
     image.setName(des_file);
     image.WriteFile();
     
